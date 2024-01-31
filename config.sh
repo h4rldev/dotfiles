78ae1d549c0856249a3611a8807ad41cf0fb4e83
h4rl <me@h4rl.xn--q9jyb4c>
2024-01-31 06:40:35 +0100

    Added update and did some setup

---
+++ config.sh
@@ -0,0 +1,1 @@
+FZF_ENABLED=true
--- install.sh
+++ install.sh
@@ -2,18 +2,24 @@
 
 VERSION="0.0.1"
 WORKING_DIR="${HOME}"
-FZF_INSTALLED=$(command -v fzf)
+
+source ./config.sh
+
 ROOT_PACKAGE_MANAGER="pacman"
+USER_PACKAGE_MANAGER="paru"
+
+SUDO_EXIST=$(command -v sudo >/dev/null)
+FZF_INSTALLED=$(command -v fzf)
+DOES_USER_PACKAGE_MANAGER_EXIST=$(command -v ${USER_PACKAGE_MANAGER})
 
 # Dirs
 CONFIG_DIR="$(pwd)/.config/"
 RESOURCES="$(pwd)/resources"
 
-
-if command -v sudo >/dev/null; then
-  sudo -K
+if [[ -n ${SUDO_EXIST} ]]; then
+  sudo -k
 else
-  alias sudo=doas
+  alias sudo="doas"
 fi
 
 clear
@@ -26,20 +32,65 @@ BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 CLEAR='\033[0m'
 
+get_packages() {
+  local INDEX=0
+  local LINE
+  local FILE
+  local -a PACKAGES
+
+  if [[ -n ${1} ]]; then
+    FILE=${1}
+  else
+    echo -e "No file provided.">&2
+    exit
+  fi
+
+  while IFS= read -r LINE; do
+    if [[ ! -z "${LINE}" ]]; then
+      PACKAGES[INDEX]="${LINE}"
+      ((INDEX++))
+    fi
+  done < "${RESOURCES}/${FILE}"
+  echo "${PACKAGES[@]}"
+}
+
+
+reset_sudo_timer() {
+  if [[ -n ${SUDO_EXIST} ]]; then
+    sudo -k
+  fi
+}
+
+sudo_trap() {
+  trap 'echo "Sudo was cancelled, exiting.."' SIGINT 
+}
+
+setup() {
+  local -a PACKAGES
+  local TEMP_DIR
+
+  PACKAGES=$(get_packages "setup.txt")
+  
+  reset_sudo_timer
+  sudo_trap
+  sudo "${ROOT_PACKAGE_MANAGER}" -S ${PACKAGES[@]}
+
+  TEMP_DIR=$(mktemp -d temp.XXX)
+  git clone "https://aur.archlinux.org/paru.git" $TEMP_DIR
+  cd $TEMP_DIR
+  makepkg -si
+  rm -fr $TEMP_DIR
+}
+
 
 install_packages() {
-  local -a PACKAGES
-  index=0
-  while IFS= read -r line
-  do
-    if [[ ! -z "$line" ]]; then
-      PACKAGES[index]="$line"
-      ((index++))
-    fi
-  done < "${RESOURCES}/pacman-packages.txt"
+  local -a PACKAGES 
+  PACKAGES=$(get_packages "pacman-packages.txt")
 
-  echo -e "${PACKAGES[@]}"
-  sudo pacman -S "${PACKAGES[@]}"
+  sudo_trap
+  reset_sudo_timer
+  echo -e "Installing: ${PACKAGES[@]}"
+  sudo ${ROOT_PACKAGE_MANAGER} -S "${PACKAGES[@]}"
 
 }
 
@@ -48,7 +99,13 @@ copy_config() {
 }
 
 update() {
-  echo -e "I update"
+  reset_sudo_timer
+  sudo_trap
+  echo -e "Updating.."
+  sudo ${ROOT_PACKAGE_MANAGER} -Syu
+  if [[ -n ${DOES_USER_PACKAGE_MANAGER_EXIST} ]]; then
+    ${USER_PACKAGE_MANAGER} -Syu
+  fi
 }
 
 full_setup() {
@@ -62,7 +119,7 @@ render_menu() {
   local PROMPT
   local RESULT
 
-  OPTIONS=("Full Setup" "Install Packages" "Copy Config" "Update" "Exit")
+  OPTIONS=("Full Setup" "Setup Prerequisites" "Install Packages" "Copy Config" "Update" "Exit")
   PROMPT="Action"
 
   if [[ ${1} != false ]]; then
@@ -83,15 +140,18 @@ render_menu() {
       full_setup
       ;;
     ${OPTIONS[1]})
-      install_packages
+      setup
       ;;
     ${OPTIONS[2]})
-      copy_config
+      install_packages
       ;;
     ${OPTIONS[3]})
-      update
+      copy_config
       ;;
     ${OPTIONS[4]})
+      update
+      ;;
+    ${OPTIONS[5]})
       exit 0
       ;;
   esac
@@ -101,7 +161,6 @@ render_menu() {
 main() {
   local FZF_INSTALL_DECISION
   local FZF_ENABLE_DECISION
-  local FZF_ENABLED
 
   if [[ -z ${FZF_INSTALLED} ]]; then
     echo -ne "${CYAN}fzf is not installed, would you like me to install it for you? ${CLEAR}(${GREEN}Y${CLEAR}/${RED}n${CLEAR}): "; read -r FZF_INSTALL_DECISION
@@ -109,17 +168,17 @@ main() {
       sudo "${ROOT_PACKAGE_MANAGER}" -S fzf
     fi 
   else
-    echo -ne "${CYAN}Would you like to use fzf for navigation? ${CLEAR}(${GREEN}Y${CLEAR}/${RED}n${CLEAR}): "; read -r FZF_ENABLE_DECISION
-    if ! [[ ${FZF_ENABLE_DECISION} =~ [nN] ]]; then
-      FZF_ENABLED=true
-    else
-      FZF_ENABLED=false
+    if ! ${FZF_ENABLED}; then
+      echo -ne "${CYAN}Would you like to use fzf for navigation? ${CLEAR}(${GREEN}Y${CLEAR}/${RED}n${CLEAR}): "; read -r FZF_ENABLE_DECISION
+      if ! [[ ${FZF_ENABLE_DECISION} =~ [nN] ]]; then
+        FZF_ENABLED=true
+        sed -i 's/FZF_ENABLED=false/FZF_ENABLED=true/' ./config.sh
+      fi
     fi
 	fi
 
   render_menu "${FZF_ENABLED}"
 }
-
 
 case $1 in 
   "--help" | "-h" | "-?")
